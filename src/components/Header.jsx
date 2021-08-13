f1bcae785a213438a5afbe05e6351d54871447f9
Ydannel <44483665+Ydannel@users.noreply.github.com>
2021-08-13 14:03:38 -0400

    added new pages
    .

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+
+const Header = () => {
+    return ( 
+        <div id='main'>
+            <div className='pr-heading'>
+                <h2>STEP YOUR</h2>
+                <h1><span>FITNESS</span>WITH US</h1>
+                <p className='details'>
+                    Build your body with professionals
+                </p>
+                <div className='header-btns'>
+                    <a href="#" className='header-btn'>GET STARTED</a>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Header;
